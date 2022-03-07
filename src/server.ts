ba2dcae4582a017c747642b4dc17eca8794005c4
Roberto <robertomelo7@hotmail.com>
2022-03-07 19:53:23 -0300

    add categories

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    console.log("route");
-
-    return res.json({ message: "Hi my name is Goku!!!" });
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!!!"));
